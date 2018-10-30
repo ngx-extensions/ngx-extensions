9914d9ec882b94e0f19002468fd3af120f57836c
José Toledo <jotatoledon@gmail.com>
2018-10-30 21:43:32 +0100

    test(screenfull): extend test suite
    * Add module tests
    * Add service tests

--- projects/screenfull/src/lib/screenfull.service.spec.ts
+++ projects/screenfull/src/lib/screenfull.service.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
+import screenfull from 'screenfull';
 
 import { ScreenfullService } from './screenfull.service';
 
 describe('ScreenService', () => {
+  const screenfullObject = screenfull && screenfull;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ScreenfullService]
@@ -15,4 +18,31 @@ describe('ScreenService', () => {
       expect(service).toBeTruthy();
     }
   ));
+
+  it('should enter fullscreen mode', () => {
+    const screenModeSpy = spyOnProperty(screenfullObject, 'isFullscreen');
+    const requestSpy = spyOn(screenfullObject, 'request').and.callFake(() =>
+      screenModeSpy.and.returnValue(true)
+    );
+    const uut = createUnitUnderTest();
+
+    uut.request();
+
+    expect(requestSpy).toHaveBeenCalled();
+    expect(uut.isFullScreenModeActive).toBeTruthy();
+  });
+
+  it('should exit fullscreen mode', () => {
+    const exitSpy = spyOn(screenfullObject, 'exit');
+    const uut = createUnitUnderTest();
+
+    uut.exit();
+
+    expect(exitSpy).toHaveBeenCalled();
+    expect(uut.isFullScreenModeActive).toBeFalsy();
+  });
+
+  function createUnitUnderTest() {
+    return new ScreenfullService();
+  }
 });
