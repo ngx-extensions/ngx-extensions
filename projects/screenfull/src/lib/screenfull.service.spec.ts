e11b90396584467670a0f6384c64d4a70bfd152b
José Toledo <jotatoledon@gmail.com>
2018-10-31 07:00:58 +0100

    test(screenfull): extend utests for screenservice
    * Replace screenfull API methods with jasmine spies

--- projects/screenfull/src/lib/screenfull.service.spec.ts
+++ projects/screenfull/src/lib/screenfull.service.spec.ts
@@ -24,8 +24,8 @@ describe('ScreenService', () => {
     const requestSpy = spyOn(screenfullObject, 'request').and.callFake(() =>
       screenModeSpy.and.returnValue(true)
     );
-    const uut = createUnitUnderTest();
 
+    const uut = createUnitUnderTest();
     uut.request();
 
     expect(requestSpy).toHaveBeenCalled();
@@ -34,12 +34,40 @@ describe('ScreenService', () => {
 
   it('should exit fullscreen mode', () => {
     const exitSpy = spyOn(screenfullObject, 'exit');
-    const uut = createUnitUnderTest();
 
+    const uut = createUnitUnderTest();
     uut.exit();
 
     expect(exitSpy).toHaveBeenCalled();
     expect(uut.isFullScreenModeActive).toBeFalsy();
+  });
+
+  it('should read the state of the screen mode and install a listener on service instance creation', () => {
+    const fullScreenSpy = spyOnProperty(screenfullObject, 'isFullscreen');
+    const onSpy = spyOn(screenfullObject, 'on');
+
+    createUnitUnderTest();
+
+    expect(onSpy).toHaveBeenCalledWith('change', jasmine.any(Function));
+    expect(fullScreenSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should correctly uninstall an event listered on "ngOnDestroy"', () => {
+    const offSpy = spyOn(screenfullObject, 'off');
+
+    const uut = createUnitUnderTest();
+    uut.ngOnDestroy();
+
+    expect(offSpy).toHaveBeenCalledWith('change', jasmine.any(Function));
+  });
+
+  it('should correctly toggle the fullscreenmode', () => {
+    const toggleSpy = spyOn(screenfullObject, 'toggle');
+
+    const uut = createUnitUnderTest();
+    uut.toggle();
+
+    expect(toggleSpy).toHaveBeenCalled();
   });
 
   function createUnitUnderTest() {
