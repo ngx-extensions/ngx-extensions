344c43431ae14d1af3113cf640d20ffab9b8659d
José Toledo <jotatoledon@gmail.com>
2018-11-16 12:11:39 +0100

    build: update build lib script
    * Fix wrong tsconfig files being used
    * Add new source folder

--- scripts/build-lib.sh
+++ scripts/build-lib.sh
@@ -14,6 +14,6 @@ echo 'Building modules...'
 
 # TODO Make use of the angular-cli built-in build process
 ./node_modules/.bin/ng-packagr -p projects/extensions/ng-package.json -c projects/extensions/tsconfig.json
-./node_modules/.bin/ng-packagr -p projects/screenfull/ng-package.json -c projects/extensions/tsconfig.json
-
+./node_modules/.bin/ng-packagr -p projects/screenfull/ng-package.json -c projects/screenfull/tsconfig.json
+./node_modules/.bin/ng-packagr -p projects/count-up.js/ng-package.json -c projects/count-up.js/tsconfig.json
 echo 'Build complete!'
